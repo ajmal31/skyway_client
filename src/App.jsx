264b17b9047447ede2eeacf787275fa85f81b88e
ajmal31 <ajmalmuhammed846@gmail.com>
2023-11-12 19:00:51 +0530

    v11 venture form almost completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 
 //import pages here
+
+//React Related imports
 import React,{useState,useEffect} from "react"
 import { BrowserRouter as Router ,Routes,Route,useLocation,Navigate } from "react-router-dom"
+
+
+//User related imports
 import UserLogin from "./pages/user/login/UserLogin"
 import UserRegister from "./pages/user/register/UserRegister"
 import Home from "./pages/user/home/Home"
 import UserSlice from "./redux/slices/UserSlice"
 import { useSelector } from "react-redux"
 import Profile from "./pages/user/profile/Profile"
+
+
+//Venture related imports
+
+import VentureLogin from "./pages/venture/Login/VentureLogin"
 
 
 function App() {
@@ -19,11 +29,19 @@ function App() {
   <Router>
   
   <Routes>
-
+       {/* User Routes */}
         <Route  path="/userLogin" element={token? <h1>sorry you are logged in</h1>: <UserLogin/>} />
         <Route path="/userRegister" element={<UserRegister/>}/>
         <Route path='/' element={(<Home/>)}/>
         <Route path='/userProfile' element={token?<Profile/>:<Navigate to={'/userLogin'} />}/>
+
+
+        {/* Venture Routes */}
+         <Route path="/venture" element={<h1>Venture side</h1>}/> 
+         <Route path="/venture/login" element={<VentureLogin/>}/>
+
+
+        {/* Admin Routes */}
         
 
 
---
+++ src/pages/venture/Login/VentureLogin.jsx
@@ -0,0 +1,19 @@
+// import here componets if its required
+import Vlogin from "../../../components/venture/Login/Vlogin"
+const VentureLogin=()=>{
+
+
+return(
+
+
+    <Vlogin/>
+    
+)
+
+
+
+
+
+}
+
+export default VentureLogin
